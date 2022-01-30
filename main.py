d8e58d2ed93b4b7e2cf2d7201b2eedfd0efc3e0e
Pander <pander@users.sourceforge.net>
2022-01-30 17:07:32 +0100

    2.4.0

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-# -*- coding: utf-8 -*-
-# Module: default
-# Author: Pander
-# Created on: 2019-03-04
-# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+'''Kodi video plugin for Intergalactic FM'''
+
+__author__ = 'Pander'
+__copyright__ = 'GPL v.3 https://www.gnu.org/copyleft/gpl.html'
 
 from json import load
-from sys import argv
 from os.path import isfile
-import requests
+import sys
 
-from urllib import urlencode
 from urlparse import parse_qsl
-
 
 import xbmcgui
 import xbmcplugin
@@ -19,12 +15,12 @@ import xbmcaddon
 from xbmc import log, LOGDEBUG
 
 __addonid__ = "plugin.audio.intergalacticfm"
-base = xbmc.translatePath('special://home/addons/{}/resources/'.format(__addonid__))
+base = xbmc.translatePath(f'special://home/addons/{__addonid__}/resources/')
 
 # Get the plugin url in plugin:// notation.
-_url = argv[0]
+_url = sys.argv[0]
 # Get the plugin handle as an integer number.
-_handle = int(argv[1])
+_handle = int(sys.argv[1])
 
 _addon = xbmcaddon.Addon()
 
@@ -34,7 +30,7 @@ def list_audios():
     """
 
     radio = 'http://radio.intergalactic.fm:80/'
-    streams = load(open(base + 'streams.json'))
+    streams = load(open(base + 'streams.json'))  # pylint:disable=consider-using-with,unspecified-encoding
     listing = []
 
     xbmcplugin.setPluginCategory(_handle, 'Live Streams')
@@ -42,7 +38,9 @@ def list_audios():
 
     for key, audio in streams.items():
         list_item = xbmcgui.ListItem(label=audio['label'])
-        list_item.setInfo(type='video', infoLabels={'genre': audio['genre'], 'plot': audio['plot'], 'tagline': audio['tagline']})
+        list_item.setInfo(type='video', infoLabels={'genre': audio['genre'],
+                                                    'plot': audio['plot'],
+                                                    'tagline': audio['tagline']})
 
         # see https://kodi.wiki/view/Movie_artwork
         # only poster, fanart and clearlogo is supported/needed
@@ -51,30 +49,30 @@ def list_audios():
 
         #log(__addonid__ + ' key: ' + key, LOGDEBUG)
         # poster 1000x1500 1:1.5 PNG
-        poster = '{}{}-poster.png'.format(base, key)
+        poster = f'{base}{key}-poster.png'
         if isfile(poster):
             art['poster'] = poster
         else: # note: specific fallback
-            art['poster'] = '{}intergalactic_radio-poster.png'.format(base)
+            art['poster'] = '{base}intergalactic_radio-poster.png'
         #log(__addonid__ + ' poster: ' + art['poster'], LOGDEBUG)
 
         # fanart 1920x1080 16:9 JPG
-        fanart = '{}{}-fanart.jpg'.format(base, key.split('_')[0])
+        fanart = f'{base}{key.split("_")[0]}-fanart.jpg'
         if isfile(fanart):
             art['fanart'] = fanart
         else: # note: specific fallback
-            art['fanart'] = '{}fanart.jpg'.format(base)
+            art['fanart'] = '{base}fanart.jpg'
         #log(__addonid__ + ' fanart: ' + art['fanart'], LOGDEBUG)
 
         # clearlogo 800x310 1:0.388 transparent PNG (is top-left corner overlay)
-        art['clearlogo'] = '{}intergalactic_radio-clearlogo.png'.format(base)
+        art['clearlogo'] = '{base}intergalactic_radio-clearlogo.png'
 
         list_item.setArt(art)
         list_item.setProperty('IsPlayable', 'true')
 
-        url = '{}{}'.format(radio, audio['file'])
+        url = f'{radio}{audio["file"]}'
         log(__addonid__ + ' url: ' + url, LOGDEBUG)
-        url = '{}?action=play&video={}'.format(_url, url)
+        url = f'{_url}?action=play&video={url}'
         log(__addonid__ + ' url: ' + url, LOGDEBUG)
         is_folder = False
 
@@ -119,7 +117,7 @@ def router(paramstring):
             # If the provided paramstring does not contain a supported action
             # we raise an exception. This helps to catch coding errors,
             # e.g. typos in action names.
-            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+            raise ValueError(f'Invalid paramstring: {paramstring}!')
     else:
         list_audios()
 
